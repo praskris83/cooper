afaa43f99309301d5ac323efe19ce38d0cc98ec4
Venky Shen <vshenbagaraju@ADFDATA.NET>
2019-01-10 12:58:02 +0530

    overall negative score calc dummy method

--- mr-cooper-hackthon/src/main/java/com/hackthon/service/PaymentDateHandler.java
+++ mr-cooper-hackthon/src/main/java/com/hackthon/service/PaymentDateHandler.java
@@ -3,10 +3,28 @@
  */
 package com.hackthon.service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.hackthon.bo.Conversation;
+import com.hackthon.bo.ConversationContext;
+import com.hackthon.bo.SentimentalPolarity;
+
 /**
  * @author prasad
  *
  */
 public class PaymentDateHandler {
+  
+  public void updateNegativeScore(ConversationContext context) {
+    int negativeScore = 0;
+    List<Conversation> conversations = context.getConversation();
+    conversations = conversations.stream().filter( c -> SentimentalPolarity.NEGATIVE.equals(c.getSentimentalPolarity())).collect(Collectors.toList());
+    for (Conversation conversation : conversations) {
+      if(SentimentalPolarity.NEGATIVE.equals(conversation.getSentimentalPolarity())) {
+        
+      }
+    }
+  }
 
 }
