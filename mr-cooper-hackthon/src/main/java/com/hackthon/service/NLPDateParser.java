6562f19adbc6f962338b446d93b21b1ec72589d3
Prasad Krishnan <praskris83@gmail.com>
2019-01-10 15:31:44 +0530

    Date NLP Handling

---
+++ mr-cooper-hackthon/src/main/java/com/hackthon/service/NLPDateParser.java
@@ -0,0 +1,45 @@
+/**
+ * 
+ */
+package com.hackthon.service;
+
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.stereotype.Service;
+
+import com.joestelmach.natty.DateGroup;
+import com.joestelmach.natty.Parser;
+
+/**
+ * @author prasad
+ *
+ */
+@Service
+public class NLPDateParser {
+
+  ThreadLocal<Parser> threadLocalParser;
+
+  @PostConstruct
+  public void init() {
+    threadLocalParser = ThreadLocal.withInitial(() -> new Parser());
+  }
+
+  public List<LocalDate> getDates(String input){
+    List<LocalDate> dates = new ArrayList<LocalDate>();
+    List<DateGroup> groups = threadLocalParser.get().parse(input);
+    for(DateGroup group:groups) {
+      List<Date> dts = group.getDates();
+      List<LocalDate> lDts= dts.stream().map(dt -> 
+        dt.toInstant().atZone(ZoneId.systemDefault()).toLocalDate()).collect(Collectors.toList());
+      dates.addAll(lDts);
+    }
+    return dates;
+  }
+}
--- mr-cooper-hackthon/src/main/java/com/hackthon/util/CommonUtils.java
+++ mr-cooper-hackthon/src/main/java/com/hackthon/util/CommonUtils.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
+import java.util.List;
 import java.util.Properties;
 
 /**
@@ -16,13 +17,19 @@ import java.util.Properties;
 public class CommonUtils {
 
   static Properties props = null;
-  static final String PROPERTY_FILE = "template.properties";
   
+  static final String PROPERTY_FILE = "template.properties";
+
   public static boolean isPayDateValid(LocalDate curDate, LocalDate newdate) {
     int days = (int) ChronoUnit.DAYS.between(curDate, newdate);
     return Math.abs(days) <= 5 && newdate.isAfter(LocalDate.now().plusDays(2));
   }
-  
+
+  public static LocalDate getNewPaymentDate(LocalDate curDate, List<LocalDate> newDates) {
+    return newDates.stream().filter(newDate -> isPayDateValid(curDate, newDate)).findFirst()
+        .orElse(null);
+  }
+
   public static void load() throws IOException {
     try (InputStream stream =
         CommonUtils.class.getClassLoader().getResourceAsStream(PROPERTY_FILE)) {
@@ -30,7 +37,7 @@ public class CommonUtils {
       props.load(stream);
     }
   }
-  
+
   public static String getTemplate(String key) throws IOException {
     if (props == null) {
       load();
